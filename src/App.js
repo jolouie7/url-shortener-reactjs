4f61b64c29094271e35f7a5f53e2ab62df45a246
Your_Name <joseph161216@gmail.com>
2020-09-17 22:32:53 -0700

    complete reactjs frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [longUrl, setLongUrl] = useState("")
+  const [shortUrl, setshortUrl] = useState("")
+  const [data, setData] = useState({})
+
+  const handleChange = (e) => {
+    setLongUrl(e.target.value)
+  }
+
+  const showRes = async () => {
+    const response = await fetch(`http://localhost:5000/`);
+    const data = await response.json()
+    setData(data);
+    console.log(data)
+  }
+
+  const createShortUrl = async () => {
+    const res = await fetch("http://localhost:5000/url", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({longUrl: longUrl}),
+    });
+    const content = await res.json()
+    setLongUrl("");
+    return content
+  }
+
+  const handleClick = async () => {
+    createShortUrl();
+    showRes();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>Enter a URL here</label>
+      <input
+        placeholder="Enter URL here"
+        value={longUrl}
+        onChange={handleChange}
+      ></input>
+      <button type="button" onClick={handleClick}>
+        Shorten
+      </button>
+      <p>{Object.keys(data).length !== 0 ? data[data.length-1].shortUrl + data[data.length-1].urlCode: null}</p>
     </div>
   );
 }
